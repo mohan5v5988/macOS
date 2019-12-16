f9a044bc2ae5a0bd10f46a46ddec4950ea4a4483
MOHAN KUMAR VELAGA <mohan5v5988@gmail.com>
2019-12-15 19:48:25 -0800

    added .aliases and oh-my-zsh.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,12 +3,11 @@
 NOW="$(date)"
 FILE=/Users/velagas/logs/backup.log
 LINE_SEP="--------------------------------"
-{
+
     echo $LINE_SEP
     echo $NOW
     echo $LINE_SEP
 
-    rsync -zarP ~/.backup/ velaga@192.168.1.17:/home/velaga/.backup/
-} >> $FILE 2>&1&
+    rsync -zarP /Users/velagas/.backup/ velaga@192.168.1.17:/home/velaga/.backup/
 
 exit 0
